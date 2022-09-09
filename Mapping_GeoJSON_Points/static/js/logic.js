90cc3ec38db106113cd64a7758a693a84abc6b23
ABorden23 <alexborden2@gmail.com>
2022-09-09 18:53:29 -0400

    deliverable 1 commit

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -8,6 +8,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// // We create the satellite layer that will be the background of our map.
+// let satellitestreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     accessToken: API_KEY
+// });
+
 // We create the dark view tile layer that will be an option for our map.
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -15,25 +22,31 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
-// Then we add our tile layer to the map.
-streets.addTo(map);
+// // Then we add our streets layer to the map.
+// streets.addTo(map);
+
+// Then we add our dark layer to the map.
+dark.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<ABorden23>/Mapping_Earthquakes_JS_APIs/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/ABorden23/Mapping_Earthquakes_JS_APIs/main/data/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
+  
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJSON(data).addTo(map);
 });
 
-// // Create a base layer that holds both maps.
-// let baseMaps = {
-//     Street: streets,
-//     Dark: dark
-//   };
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark,
+    Satellite: satellitestreets
+  };
 
+//   // Another teqnique to creating map
 //   // Create the map object with center, zoom level and default layer.
 // let map = L.map('mapid', {
 //     center: [30, 30],
@@ -41,8 +54,8 @@ L.geoJSON(data).addTo(map);
 //     layers: [streets]
 // })
 
-// // Pass our map layers into our layers control and add the layers control to the map.
-// L.control.layers(baseMaps).addTo(map);
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // // Accessing the Toronto airline routes GeoJSON URL.
 // let torontoData = "https://raw.githubusercontent.com/<ABorden23e>/Mapping_Earthquakes_JS_API/main/torontoRoutes.json";
